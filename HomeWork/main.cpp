2a9671ca9085c7814131e1ebd38ffbcabd083b41
Rustam <refractory40@gmail.com>
2023-06-06 18:56:19 +0900

    Update README.md

--- HomeWork/main.cpp
+++ HomeWork/main.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "rus");
+	setlocale(LC_ALL, "rus");
 
-    string s = "кругу"; 
-    string line; // строка из файла
-    string fileName = "file.txt";
+	string s = "кругу";
+	string line; // строка из файла
+	string fileName = "file.txt";
 
     int lineNum = 0;
     bool found = false;
@@ -32,6 +32,26 @@ int main() {
     }
     else if (!file.is_open()) cout << "\t|Не удалось открыть файл|" << endl;
     else cout << "\t\t|Файл закрыт|" << endl;
+	int lineNum = 0;
+	bool found = false;
+	ifstream file(fileName);
+	if (file.is_open()) {
+		cout << "\t\t|Файл открыт|" << endl;
+		cout << "Слова для поиска: \"" << s << "\"" << endl;
+		while (getline(file, line)) {
+			lineNum++;
+			if (line.find(s) != string::npos) {
+				cout << "Номер строки: " << lineNum << "  \| " << line << endl;
+				found = true;
+			}
+		}
+		file.close();
+		if (!found) {
+			cout << "По результату \" " << s << " \" ничего не найдено." << endl;
+		}
+	}
+	else if (!file.is_open()) cout << "\t|Не удалось открыть файл|" << endl;
+	else cout << "\t\t|Файл закрыт|" << endl;
 
-    return 0;
+	return 0;
 }
