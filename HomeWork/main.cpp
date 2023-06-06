a31e6c692492fff00b93781811f0f2fdad8e68eb
Rustam <refractory40@gmail.com>
2023-06-06 19:05:21 +0900

    Update README.md

--- HomeWork/main.cpp
+++ HomeWork/main.cpp
@@ -10,30 +10,9 @@ int main() {
 	string s = "кругу";
 	string line; // строка из файла
 	string fileName = "file.txt";
-
-    int lineNum = 0;
-    bool found = false;
-    ifstream file(fileName);
-    if (file.is_open()) {
-        cout << "\t\t|Файл открыт|" << endl;
-        cout << "Cлово для поиска: \""<<s << "\"" << endl;
-        while (getline(file, line)) {
-            lineNum++;
-            if (line.find(s) != string::npos) {
-                cout << "Номер строки: " << lineNum << "  \| " << line << endl;
-                found = true;
-            }
-        }
-        file.close();
-
-        if (!found) {
-            cout << "По результату \" " << s <<" \" ничего не найденно." << endl;
-        }
-    }
-    else if (!file.is_open()) cout << "\t|Не удалось открыть файл|" << endl;
-    else cout << "\t\t|Файл закрыт|" << endl;
 	int lineNum = 0;
 	bool found = false;
+
 	ifstream file(fileName);
 	if (file.is_open()) {
 		cout << "\t\t|Файл открыт|" << endl;
