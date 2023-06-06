60e766a47bbb766f2f0f2f7b23e11461a8b23ccc
Rustam <115391516+IncludeSpecial@users.noreply.github.com>
2023-06-06 17:57:41 +0900

    Update README.md

--- HomeWork/main.cpp
+++ HomeWork/main.cpp
@@ -1,40 +1,36 @@
+#include <fstream>      
+#include <string>       
 #include <iostream>
-#include <fstream>      //для работы с файлами
-#include <string>       //для работы со строками
 
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "rus");
 
-    string s = "све";                 // (символ\слово\слог) строка для поиска 
+    string s = "кругу"; 
     string line; // строка из файла
-    string fileName = "file.txt";       //название файла
+    string fileName = "file.txt";
 
-    int lineNum = 0;                    // для вывода номера строки
-    bool found = false;                 // если ничего не найденно
-    ifstream file(fileName);            // открытие файла
-
-
-    if (file.is_open()) {               // проверка на успешное открытие файла
+    int lineNum = 0;
+    bool found = false;
+    ifstream file(fileName);
+    if (file.is_open()) {
         cout << "\t\t|Файл открыт|" << endl;
-        cout << "Cлово для поиска: "<<s <<endl;
-        while (getline(file, line)) {               // считывание каждой строки из файла
+        cout << "Cлово для поиска: \""<<s << "\"" << endl;
+        while (getline(file, line)) {
             lineNum++;
-            if (line.find(s) != string::npos) {     // если строка содержит искомую подстроку
-                cout << "Номер строки: " << lineNum << "  \| " << line << endl; // вывод строки на экран
+            if (line.find(s) != string::npos) {
+                cout << "Номер строки: " << lineNum << "  \| " << line << endl;
                 found = true;
             }
         }
-        file.close();                   // закрытие файла
+        file.close();
 
         if (!found) {
-            cout << "По результату \" " << s <<" \" ничего не найденно." << endl; // Вывод результата
+            cout << "По результату \" " << s <<" \" ничего не найденно." << endl;
         }
     }
-    else if (!file.is_open()) {
-        cout << "\t|Не удалось открыть файл|" << endl;      // вывод сообщения об ошибке
-    }
+    else if (!file.is_open()) cout << "\t|Не удалось открыть файл|" << endl;
     else cout << "\t\t|Файл закрыт|" << endl;
 
     return 0;
